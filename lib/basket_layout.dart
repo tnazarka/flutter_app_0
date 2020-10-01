c7a8ce067742e9530f27607a82f3b607c87aeefc
tnazarka <ferbmfines@gmail.com>
2020-10-01 15:25:48 +0300

    сделал, но не знаю как обновить  FirstRoute посел выхода из корзины, и не знаю как сделать чтобы обща стоимость в корзине находилась снизу статично и чтобы виджет подстаивался под размер экрана

--- lib/basket_layout.dart
+++ lib/basket_layout.dart
@@ -40,144 +40,156 @@ class _BasketRouteState extends State<BasketRoute> {
           title: Text('Корзина'),
           backgroundColor: Colors.green[600],
         ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Container(
-              height: 520,
-              child: ListView.builder(
-                  itemCount: quantitys.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Container(
+        body: Container(
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  height: 520,
+                  child: ListView.builder(
+                      itemCount: quantitys.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Container(
 //                      height: 130,
-                      child: Padding(
-                        padding: const EdgeInsets.only(
-                            left: 15.0, right: 15, top: 8),
-                        child: Material(
-                          borderRadius: BorderRadius.all(Radius.circular(14)),
-                          elevation: 5.0,
-                          child: Container(
-                            child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: <Widget>[
-                                Container(
-                                  height: 100,
-                                  width: 70,
-                                  child: Image.network(
-                                      products[quantitys[index].productId]
-                                          .imageurl_),
-                                ),
-                                InkWell(
-                                  onTap: () {},
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(left: 8.0),
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: <Widget>[
-                                        SizedBox(height: 20),
-                                        Container(
-                                            width: 200,
-                                            child: Text(
-                                              products[quantitys[index]
-                                                      .productId]
-                                                  .title,
-                                              style: TextStyle(
-                                                  fontSize: 16,
-                                                  fontWeight: FontWeight.bold),
-                                            )),
-                                        SizedBox(height: 10),
-                                        Text(
-                                            "\₽" +
-                                                products[quantitys[index]
-                                                        .productId]
-                                                    .pricetext
-                                                    .toString(),
-                                            style: TextStyle(
-                                              fontSize: 16,
-                                            ))
-                                      ],
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                                left: 15.0, right: 15, top: 8),
+                            child: Material(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(14)),
+                              elevation: 5.0,
+                              child: Container(
+                                child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: <Widget>[
+                                    Container(
+                                      height: 100,
+                                      width: 70,
+                                      child: Image.network(
+                                          products[quantitys[index].productId]
+                                              .imageurl_),
                                     ),
-                                  ),
-                                ),
-                                Column(
-                                  //child: Row(
-                                  children: [
-                                    Padding(
-                                      padding: const EdgeInsets.all(1.0),
-                                      child: IconButton(
-                                        icon: Icon(Icons.arrow_drop_up),
-                                        onPressed: () {
-                                          setState(() {
-                                            _numberQuantity += 1;
-                                            _numberQuantity =
-                                                quantitys[index].quantity += 1;
-                                            // Price_check();
-                                          });
-
-//                                    quantitys[index].quantity += 1;
-                                        },
+                                    InkWell(
+                                      onTap: () {},
+                                      child: Padding(
+                                        padding:
+                                            const EdgeInsets.only(left: 8.0),
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: <Widget>[
+                                            SizedBox(height: 20),
+                                            Container(
+                                                width: 200,
+                                                child: Text(
+                                                  products[quantitys[index]
+                                                          .productId]
+                                                      .title,
+                                                  style: TextStyle(
+                                                      fontSize: 16,
+                                                      fontWeight:
+                                                          FontWeight.bold),
+                                                )),
+                                            SizedBox(height: 10),
+                                            Text(
+                                                "\₽" +
+                                                    products[quantitys[index]
+                                                            .productId]
+                                                        .pricetext
+                                                        .toString(),
+                                                style: TextStyle(
+                                                  fontSize: 16,
+                                                ))
+                                          ],
+                                        ),
                                       ),
                                     ),
-                                    Padding(
-                                      padding: const EdgeInsets.all(1.0),
-                                      child: Text(
-                                          quantitys[index].quantity.toString()),
-                                    ),
-                                    Padding(
-                                        padding: const EdgeInsets.all(1.0),
-                                        child: IconButton(
-                                          icon: Icon(Icons.arrow_drop_down),
-                                          onPressed: () {
-                                            setState(() {
-                                              if (quantitys[index].quantity <=
-                                                  1) {
-                                                products[quantitys[index]
-                                                            .productId]
-                                                        .checkIcon =
-                                                    Icons.add_shopping_cart;
+                                    Column(
+                                      //child: Row(
+                                      children: [
+                                        Padding(
+                                          padding: const EdgeInsets.all(1.0),
+                                          child: IconButton(
+                                            icon: Icon(Icons.arrow_drop_up),
+                                            onPressed: () {
+                                              setState(() {
+                                                _numberQuantity += 1;
+                                                _numberQuantity =
+                                                    quantitys[index].quantity +=
+                                                        1;
+                                                // Price_check();
+                                              });
+
+//                                    quantitys[index].quantity += 1;
+                                            },
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.all(1.0),
+                                          child: Text(quantitys[index]
+                                              .quantity
+                                              .toString()),
+                                        ),
+                                        Padding(
+                                            padding: const EdgeInsets.all(1.0),
+                                            child: IconButton(
+                                              icon: Icon(Icons.arrow_drop_down),
+                                              onPressed: () {
+                                                setState(() {
+                                                  if (quantitys[index]
+                                                          .quantity <=
+                                                      1) {
+                                                    products[quantitys[index]
+                                                                .productId]
+                                                            .checkIcon =
+                                                        Icons.add_shopping_cart;
 //                                                print("сменили иконку для: " +
 //                                                    products[quantitys[index]
 //                                                            .productId]
 //                                                        .title);
-                                                quantitys.removeAt(index);
-                                              } else {
-                                                _numberQuantity -= 1;
-                                                _numberQuantity =
-                                                    quantitys[index].quantity -=
-                                                        1;
-                                                // Price_check();
-                                              }
-                                            });
+                                                    quantitys.removeAt(index);
+                                                  } else {
+                                                    _numberQuantity -= 1;
+                                                    _numberQuantity =
+                                                        quantitys[index]
+                                                            .quantity -= 1;
+                                                    // Price_check();
+                                                  }
+                                                });
 
 //                                      quantitys[index].quantity -= 1;
-                                          },
-                                        )),
+                                              },
+                                            )),
+                                      ],
+                                    ),
                                   ],
                                 ),
-                              ],
+                              ),
                             ),
                           ),
-                        ),
+                        );
+                      }),
+                ),
+                Align(
+                    alignment: Alignment.bottomRight,
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 30,
+                      color: Colors.green[600],
+                      child: Text(
+                        "Итог:" + _price_uant.toString(),
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.left,
                       ),
-                    );
-                  }),
+                    )),
+              ],
             ),
-            Align(
-                alignment: Alignment.bottomRight,
-                child: Container(
-                  alignment: Alignment.center,
-                  height: 30,
-                  color: Colors.green[600],
-                  child: Text(
-                    "Итог:" + _price_uant.toString(),
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    textAlign: TextAlign.left,
-                  ),
-                )),
-          ],
+          ),
         ));
   }
 }
